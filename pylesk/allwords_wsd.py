de299613a5228db5eedc2845ea3ec2aec18fde1c
subramaniam97 <nitksubbu@gmail.com>
2017-10-03 17:41:08 +0530

    lesk code initial

---
+++ pylesk/allwords_wsd.py
@@ -0,0 +1,49 @@
+from string import punctuation
+
+from nltk import word_tokenize, pos_tag
+from nltk.corpus import wordnet as wn
+from nltk.corpus import stopwords
+from lesk import adapted_lesk
+from utils import lemmatize, lemmatize_sentence
+
+"""
+This is a module for all-words full text WSD
+
+This would involve:
+Step 1: First tokenize your text such that each token is separated by whitespace
+Step 2: Iterates through the tokens and only disambiguate the content words.
+"""
+
+stopwords = stopwords.words('english') + list(punctuation)
+
+def disambiguate(sentence, algorithm=adapted_lesk,
+                 context_is_lemmatized=False, similarity_option='path',
+                 keepLemmas=False, prefersNone=True):
+    tagged_sentence = []
+    # Pre-lemmatize the sentnece before WSD
+    if not context_is_lemmatized:
+        surface_words, lemmas, morphy_poss = lemmatize_sentence(sentence, keepWordPOS=True)
+        lemma_sentence = " ".join(lemmas)
+    else:
+        lemma_sentence = sentence # TODO: Miss out on POS specification, how to resolve?
+    for word, lemma, pos in zip(surface_words, lemmas, morphy_poss):
+        if lemma not in stopwords: # Checks if it is a content word
+            try:
+                wn.synsets(lemma)[0]
+                synset = algorithm(lemma_sentence, lemma, pos=pos, context_is_lemmatized=True)
+            except: # In case the content word is not in WordNet
+                synset = '#NOT_IN_WN#'
+        else:
+            synset = '#STOPWORD/PUNCTUATION#'
+        if keepLemmas:
+            tagged_sentence.append((word, lemma, synset))
+        else:
+            tagged_sentence.append((word, synset))
+    # Change #NOT_IN_WN# and #STOPWORD/PUNCTUATION# into None.
+    if prefersNone and not keepLemmas:
+        tagged_sentence = [(word, None) if str(tag).startswith('#')
+                           else (word, tag) for word, tag in tagged_sentence]
+    if prefersNone and keepLemmas:
+        tagged_sentence = [(word, lemma, None) if str(tag).startswith('#')
+                           else (word, lemma, tag) for word, lemma, tag in tagged_sentence]
+    return tagged_sentence
---
+++ pylesk/lesk.py
@@ -0,0 +1,146 @@
+import string
+from itertools import chain
+
+from nltk.corpus import wordnet as wn
+from nltk.corpus import stopwords
+from nltk import word_tokenize, pos_tag
+from utils import lemmatize, porter, lemmatize_sentence, synset_properties
+import allwords_wsd 
+
+EN_STOPWORDS = stopwords.words('english') + list(string.punctuation)
+
+def compare_overlaps(context, synsets_signatures, \
+                     nbest=False, keepscore=False, normalizescore=False):
+    """
+    Calculates overlaps between the context sentence and the synset_signture
+    and returns a ranked list of synsets from highest overlap to lowest.
+    """
+    overlaplen_synsets = [] # a tuple of (len(overlap), synset).
+    for ss in synsets_signatures:
+        overlaps = set(synsets_signatures[ss]).intersection(context)
+        overlaplen_synsets.append((len(overlaps), ss))
+
+    # Rank synsets from highest to lowest overlap.
+    ranked_synsets = sorted(overlaplen_synsets, reverse=True)
+
+    # Normalize scores such that it's between 0 to 1.
+    if normalizescore:
+        total = float(sum(i[0] for i in ranked_synsets))
+        ranked_synsets = [(i/total,j) for i,j in ranked_synsets]
+
+    if not keepscore: # Returns a list of ranked synsets without scores
+        ranked_synsets = [i[1] for i in sorted(overlaplen_synsets, \
+                                               reverse=True)]
+
+    if nbest: # Returns a ranked list of synsets.
+        return ranked_synsets
+    else: # Returns only the best sense.
+        return ranked_synsets[0]
+
+def simple_signature(ambiguous_word, pos=None, lemma=True, stem=False, \
+                     hyperhypo=True, stop=True):
+    """
+    Returns a synsets_signatures dictionary that includes signature words of a
+    sense from its:
+    (i)   definition
+    (ii)  example sentences
+    (iii) hypernyms and hyponyms
+    """
+    synsets_signatures = {}
+    for ss in wn.synsets(ambiguous_word):
+        try: # If POS is specified.
+            if pos and str(ss.pos()) != pos:
+                continue
+        except:
+            if pos and str(ss.pos) != pos:
+                continue
+        signature = []
+        # Includes definition.
+        ss_definition = synset_properties(ss, 'definition')
+        signature+=ss_definition
+        # Includes examples
+        ss_examples = synset_properties(ss, 'examples')
+        signature+=list(chain(*[i.split() for i in ss_examples]))
+        # Includes lemma_names.
+        ss_lemma_names = synset_properties(ss, 'lemma_names')
+        signature+= ss_lemma_names
+
+        # Optional: includes lemma_names of hypernyms and hyponyms.
+        if hyperhypo == True:
+            ss_hyponyms = synset_properties(ss, 'hyponyms')
+            ss_hypernyms = synset_properties(ss, 'hypernyms')
+            ss_hypohypernyms = ss_hypernyms+ss_hyponyms
+            signature+= list(chain(*[i.lemma_names() for i in ss_hypohypernyms]))
+
+        # Optional: removes stopwords.
+        if stop == True:
+            signature = [i for i in signature if i not in EN_STOPWORDS]
+        # Lemmatized context is preferred over stemmed context.
+        if lemma == True:
+            signature = [lemmatize(i) for i in signature]
+        # Matching exact words may cause sparsity, so optional matching for stems.
+        if stem == True:
+            signature = [porter.stem(i) for i in signature]
+        synsets_signatures[ss] = signature
+
+    return synsets_signatures
+
+
+def adapted_lesk(context_sentence, ambiguous_word, \
+                pos=None, lemma=True, stem=True, hyperhypo=True, \
+                stop=True, context_is_lemmatized=False, \
+                nbest=False, keepscore=False, normalizescore=False):
+    
+    # Ensure that ambiguous word is a lemma.
+    ambiguous_word = lemmatize(ambiguous_word)
+    # If ambiguous word not in WordNet return None
+    if not wn.synsets(ambiguous_word):
+        return None
+    # Get the signatures for each synset.
+    ss_sign = simple_signature(ambiguous_word, pos, lemma, stem, hyperhypo)
+    for ss in ss_sign:
+        # Includes holonyms.
+        ss_mem_holonyms = synset_properties(ss, 'member_holonyms')
+        ss_part_holonyms = synset_properties(ss, 'part_holonyms')
+        ss_sub_holonyms = synset_properties(ss, 'substance_holonyms')
+        # Includes meronyms.
+        ss_mem_meronyms = synset_properties(ss, 'member_meronyms')
+        ss_part_meronyms = synset_properties(ss, 'part_meronyms')
+        ss_sub_meronyms = synset_properties(ss, 'substance_meronyms')
+        # Includes similar_tos
+        ss_simto = synset_properties(ss, 'similar_tos')
+
+        related_senses = list(set(ss_mem_holonyms+ss_part_holonyms+
+                                  ss_sub_holonyms+ss_mem_meronyms+
+                                  ss_part_meronyms+ss_sub_meronyms+ ss_simto))
+
+        signature = list([j for j in chain(*[synset_properties(i, 'lemma_names')
+                                             for i in related_senses])
+                          if j not in EN_STOPWORDS])
+
+    # Lemmatized context is preferred over stemmed context
+    if lemma == True:
+        signature = [lemmatize(i) for i in signature]
+    # Matching exact words causes sparsity, so optional matching for stems.
+    if stem == True:
+        signature = [porter.stem(i) for i in signature]
+    # Adds the extended signature to the simple signatures.
+    ss_sign[ss]+=signature
+
+    # Disambiguate the sense in context.
+    if context_is_lemmatized:
+        context_sentence = context_sentence.split()
+    else:
+        context_sentence = lemmatize_sentence(context_sentence)
+    best_sense = compare_overlaps(context_sentence, ss_sign, \
+                                    nbest=nbest, keepscore=keepscore, \
+                                    normalizescore=normalizescore)
+    return best_sense
+
+if __name__ == "__main__":
+    synonyms = []
+    synset = allwords_wsd.disambiguate('the east bank circulates a lot of water')
+    print(synset)
+    for l in synset[2][1].lemmas():
+        synonyms.append(l.name())
+    print(set(synonyms))
